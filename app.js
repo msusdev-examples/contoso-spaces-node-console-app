60c8f969ba0c2b9494fcc25f36df9bb30a4014b1
Sidney Andrews <v-sandrews@microsoft.com>
2022-02-22 15:11:47 -0500

    Add colors

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ const options = program.opts();
 
 var seatFilter = options.seats ?? 0;
 if (options.seats) {
-    console.log(`Seat Filter: ${String(seatFilter).padStart(2, '0')}`);
+    console.log(chalk.yellow(`Seat Filter: ${String(seatFilter).padStart(2, '0')}`));
 }
 
 for(var location of data.locations.filter(l => l.rooms.some(r => r.seats >= seatFilter))) {
     console.log();
-    console.log(location.name);
+    console.log(chalk.red(location.name));
     console.log(location.mailingAddress);
-    console.log("Rooms:");
-    console.log(`\t${align('Description', 25, 'right')}\t${align('Seats', 2, 'right')}\t${align('MonthlyRate', 8, 'left')}`);
+    console.log(chalk.blue("Rooms:"));
+    console.log(chalk.gray(`\t${align('Description', 25, 'right')}\t${align('Seats', 2, 'right')}\t${align('MonthlyRate', 8, 'left')}`));
     for (var room of location.rooms.filter(r => r.seats >= seatFilter)) {
         console.log(`\t${align(room.description, 25, 'right')}\t${align(room.seats, 2, 'right')}\t${align(accounting.formatMoney(room.monthlyRate), 8, 'left')}`);
     }
